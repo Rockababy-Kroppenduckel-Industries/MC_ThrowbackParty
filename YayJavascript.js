04b34e0f05d9eb3b9ea8b1c766f8768a5eea3165
RealToughCandy <barncatdigital@gmail.com>
2017-09-27 02:44:46 -0500

    Delete YayJavascript.js

--- YayJavascript.js
+++
@@ -1,6 +0,0 @@
-/* Dev 2! Yeah, I'm talking to YOU! We at Rockababy-Kroppenduckel have heard a lot of bad
-things about JavaScript. Besides, we don't really need it with this Web 1.0 aesthetic.
-I need you to remove this entire file.
-In other words, I need to let my boss know who to blame when he changes his mind on this!
-Now go Git 'er done!
--Ms. Kroppenduckel */
